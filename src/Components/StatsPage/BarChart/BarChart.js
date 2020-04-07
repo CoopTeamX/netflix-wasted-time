fc46d743ddffcc376c1eaf9f0b525f5ffe711f2e
Thomas Caudrelier <dev.caudrelier@gmail.com>
2020-04-07 23:43:28 +0200

    Display hours/minutes label tooltip (#29)

--- src/Components/StatsPage/BarChart/BarChart.js
+++ src/Components/StatsPage/BarChart/BarChart.js
@@ -2,10 +2,27 @@ import React from "react";
 import { Bar } from 'react-chartjs-2';
 
 export default class BarChart extends React.Component {
-  render() {
-    // you only have to set those values thanks to this.props
+  needToConvertToHours = () => {
+    var i = 0;
+    while (i < this.props.data.values.length) {
+      if(this.props.data.values[i] > 120) return true;
+      i++;
+    }
 
-    const data = {
+    return false;
+  }
+  
+  componentDidMount() {
+    if (this.needToConvertToHours()) {
+      this.props.data.values = this.props.data.values.map(value => Math.trunc(value / 60));
+      this.forceUpdate();
+    }
+  }
+
+  render() {
+    const self = this;
+
+    var data = {
       labels: this.props.data.labels,
       datasets: [{
         backgroundColor: 'rgba(255,255,255)',
@@ -52,13 +69,24 @@ export default class BarChart extends React.Component {
         }]
       },
 
-      maintainAspectRatio: true
+      maintainAspectRatio: true,
+
+      tooltips: {
+        enabled: true,
+        mode: 'single',
+        callbacks: {
+          label: function(tooltipItems, data) { 
+            const suffix = self.needToConvertToHours() ? ' h' : ' min';
+              return tooltipItems.yLabel + suffix;
+          }
+        }
+      },
     };
 
     return (
       <div className="chart stats--bar-chart">
         <div className="stats--title">{this.props.title}</div>
-        <div className="stats--legend-bar">Minutes</div>
+        <div className="stats--legend-bar">{this.needToConvertToHours() ? "Hours" : "Minutes"}</div>
         <Bar
           data={data}
           options={options}
